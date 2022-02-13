05ae4b75759f67a875d4aebcec96887904d3873f
Douglas Cardoso <douglascf12@live.com>
2022-02-13 12:33:38 -0300

    Criar model

---
+++ MeetupBRQ/Model/CharactersModel.swift
@@ -0,0 +1,15 @@
+//
+//  CharactersModel.swift
+//  MeetupBRQ
+//
+//  Created by Douglas Cardoso on 13/02/22.
+//
+
+import Foundation
+
+struct Characters: Codable {
+    let name: String
+    let telephone: String
+    let address: String
+    let image: String
+}
