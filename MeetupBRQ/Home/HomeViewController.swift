7bdf20f98390665a672f6894962b9a6c2f136f76
Douglas Cardoso <douglascf12@live.com>
2022-02-13 13:12:10 -0300

    Configuração da Home em ViewCode com TableView

--- MeetupBRQ/Home/HomeViewController.swift
+++ MeetupBRQ/Home/HomeViewController.swift
@@ -11,11 +11,65 @@ import UIKit
 class HomeViewController: UIViewController {
 
     var presenter: HomePresenterProtocol?
+    var messageLabel: UILabel!
+    
+    lazy var tableView: UITableView = {
+        var tableView = UITableView()
+        if #available(iOS 13.0, *) {
+            tableView = UITableView(frame: .zero, style: .grouped)
+        }
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.separatorInset = .zero
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        if #available(iOS 15, *) {
+            tableView.sectionHeaderTopPadding = 0
+        }
+        
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Vila do Chaves"
+        
+        messageLabel = UILabel()
+        messageLabel.text = "Ainda não há dado para ser exibido."
+        messageLabel.textAlignment = .center
+        messageLabel.numberOfLines = 0
+        
+        tableView.backgroundView = messageLabel
+        
+        setupLayout()
     }
+    
+    private func setupLayout() {
+        view.addSubview(tableView)
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+}
 
+// MARK: - UITableViewDelegate, UITableViewDataSource
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
 
 extension HomeViewController: HomeViewProtocol {
