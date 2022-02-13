a3bc3b47700d93228535767694b968f5c72ce228
Douglas Cardoso <douglascf12@live.com>
2022-02-13 13:10:50 -0300

    Iniciar app com Navigation Controller

--- MeetupBRQ/SceneDelegate.swift
+++ MeetupBRQ/SceneDelegate.swift
@@ -17,9 +17,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let homeViewController = HomeRouter.createModule()
+        let navigationController = UINavigationController(rootViewController: homeViewController)
         window = UIWindow(frame: .zero)
         window?.makeKeyAndVisible()
-        window?.rootViewController = homeViewController
+        window?.rootViewController = navigationController
         window?.windowScene = windowScene
     }
 
